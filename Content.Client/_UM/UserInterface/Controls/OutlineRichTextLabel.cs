60f9e7152613093a96a68e7226a104e18d307da4
Mora <trixxedheart@gmail.com>
2026-07-08 09:48:38 -0500

    Optimize OutlineRichText

--- Content.Client/_UM/UserInterface/Controls/OutlineRichTextLabel.cs
+++ Content.Client/_UM/UserInterface/Controls/OutlineRichTextLabel.cs
@@ -14,7 +14,24 @@ public sealed class OutlineRichTextLabel : RichTextLabel
 
     private ShaderInstance? _outlineShader;
 
-    public int Thickness;
+    // _Starfall Start
+    private int _thickness = 2;
+    private Vector2[] _outlineOffsets = Array.Empty<Vector2>();
+
+    // Only change thickness so we aren't updating it every frame
+    public int Thickness
+    {
+        get => _thickness;
+        set
+        {
+            if (_thickness == value)
+                return;
+
+            _thickness = value;
+            _outlineOffsets = BuildOutlineOffsets();
+        }
+    }
+    // _Starfall End
 
     public OutlineRichTextLabel(int thickness = 2)
     {
@@ -24,7 +41,8 @@ public sealed class OutlineRichTextLabel : RichTextLabel
         Thickness = thickness;
     }
 
-    private List<Vector2> BuildOutlineOffsets()
+    // _Starfall Start
+    private Vector2[] BuildOutlineOffsets()
     {
         var list = new List<Vector2>();
 
@@ -40,27 +58,29 @@ public sealed class OutlineRichTextLabel : RichTextLabel
             }
         }
 
-        return list;
+        return list.ToArray();
     }
+    // _Starfall End
 
 
     protected override void Draw(DrawingHandleScreen handle)
     {
-        var offsets = BuildOutlineOffsets();
+        // _Starfall: we dont need to rebuild this every frame, we just do it when thickness actually changes
+        // var offsets = BuildOutlineOffsets();
 
         handle.UseShader(_outlineShader);
 
         var originalTransform = handle.GetTransform();
 
-        foreach (var o in offsets)
-        {
-            float scaleX = originalTransform.M11;
-            float scaleY = originalTransform.M22;
+        // _Starfall: Handle outline thickness outside of foreach
+        var invScale = new Vector2(
+            1f / originalTransform.M11,
+            1f / originalTransform.M22);
 
-            Vector2 scaledOffset = new Vector2(
-                o.X / scaleX,
-                o.Y / scaleY
-            );
+        // _Starfall: Iterates the cached array
+        foreach (var offset in _outlineOffsets)
+        {
+            var scaledOffset = offset * invScale;
 
             var offsetMatrix = Matrix3x2.CreateTranslation(scaledOffset);
 
